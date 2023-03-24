c04893c34e9baeff13b39ed042a14c6f8ef5a2a1
omprakash-singh <omprakashingh11009@gmail.com>
2023-03-24 15:35:27 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/',
     pathMatch: 'full'
   },
+  {
+    path: 'auth/login',
+    loadChildren: ()=> import('./auth/login/login.module').then(m=>m.LoginPageModule)
+  }, 
+  {
+    path: 'auth/register',
+    loadChildren: ()=> import('./auth/register/register.module').then(m=> m.RegisterPageModule)
+  },
+  {
+    path: 'auth/forgotten-password',
+    loadChildren: ()=> import('./auth/forgotten-password/forgotten-password.module').then(m=> m.ForgottenPasswordPageModule)
+  }
+
 ];
 
 @NgModule({
