19dd9c63c8833e9065d7d185feb8f7d39082a92f
omprakash-singh <omprakashingh11009@gmail.com>
2023-03-25 19:41:54 +0530

    working on user

--- src/app/HomePages/user/user.page.ts
+++ src/app/HomePages/user/user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService, private router:Router) { }
+
+  Readmore: boolean = false;
+  longText:string ="";
 
   ngOnInit() {
+  this.longText = `This is long paragraph text containing several words continued. An example of implementing dynamically limit long text Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita optio temporibus laborum? Atque deleniti autem nam vero in corrupti, ipsam praesentium doloribus nulla numquam cum, inventore nisi exercitationem quisquam et mollitia? Corrupti, libero blanditiis fugiat quae, repellat molestiae iure aut incidunt reprehenderit, sequi eius eveniet ipsum dolore quia! Sunt consequuntur veniam reiciendis optio quas doloribus ipsam iste repudiandae, iure natus illo ea magnam ducimus excepturi ipsum possimus fugit sint unde aperiam qui consectetur quisquam ut aut esse. Adipisci dignissimos, soluta autem nesciunt quis repellendus dolore voluptates laborum placeat sed? Quasi iusto accusantium molestias perferendis. Repellendus harum natus magni dignissimos? Reprehenderit.`;
   }
+
+  onLogout() {
+    this.authService.signOut().subscribe(()=> {
+      this.router.navigate(['auth/login'])
+    })
+  }
+
 
 }
